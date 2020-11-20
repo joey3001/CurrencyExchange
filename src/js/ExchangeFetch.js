226f82bde8ca3af057e77ded545e9d20ad259615
Joey3001 <josephnero111@gmail.com>
2020-11-20 10:45:12 -0800

    Add try/catch block to currencyCall

--- src/js/ExchangeFetch.js
+++ src/js/ExchangeFetch.js
@@ -1,7 +1,14 @@
 export default class ExchangeFetch {
 
   static async currencyCall() {
-    const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-    console.log(response);
+    try { 
+      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/EUR`);
+      if (!response.ok) {
+        throw Error(response.statusText); 
+      }
+    } catch(error) {
+      alert(error.message);
+    }
   }
 }
+
