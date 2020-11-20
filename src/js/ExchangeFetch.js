3c711246863d4df9e8b7d146b58c069b2c92959f
Joey3001 <josephnero111@gmail.com>
2020-11-20 14:01:03 -0800

    Attempt to fix error handling bug

--- src/js/ExchangeFetch.js
+++ src/js/ExchangeFetch.js
@@ -3,24 +3,28 @@ export default class ExchangeFetch {
 
   static async currencyCall() {
     try { 
-      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD/`);
+      const response = await fetch (`https://v6.exchangerate-pi.com/v6/${process.env.API_KEY}/latest/KGB/`);
       if (!response.ok) {
         throw Error(response.statusText); 
       }
       return response.json();
     } 
     catch(error) {
+      console.log('maybe here')
       return(error.message);
     }
   }
 
   static getElements(response, money, currency) {
-    if (response.conversion_rates) {
-    $('#output').text((parseFloat(money)*response.conversion_rates[currency]).toFixed(2));
+    console.log(response);
+    if (response.conversion_rates.currency) {
+      $('#output').text((parseFloat(money)*response.conversion_rates[currency]).toFixed(2));
+    }
+    else if (response.conversion_rates) {
+      $('#output').text('That is not a currency supported by this application. Please try another currency');
     }
     else {
-    console.log(response);
-    $('#output').text(`There was the following error: ${response}`);
+      $('#output').text(`There was the following error: ${response}`);
     }
   }
 
